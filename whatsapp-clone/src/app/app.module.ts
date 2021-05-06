ffae4d9cd20d0c5c8231e190c382b5a2b3a60dd3
sjkadali <skadalikat@gmail.com>
2021-05-06 11:11:26 -0400

    updates to app-component.html, .scss

--- whatsapp-clone/src/app/app.module.ts
+++ whatsapp-clone/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainContainerComponent } from './components/main-container/main-container.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SidebarContentComponent } from './components/sidebar-content/sidebar-content.component';
-import { ChatAreaComponent } from './components/chat-area/chat-area.component';
-import { ChatDefaultPageComponent } from './components/chat-default-page/chat-default-page.component';
-import { ChatRoomComponent } from './components/chat-room/chat-room.component';
+import { SidebarComponent } from './components/main-container/sidebar/sidebar.component';
+import { SidebarContentComponent } from './components/main-container/sidebar/sidebar-content/sidebar-content.component';
+import { ChatAreaComponent } from './components/main-container/chat-area/chat-area.component';
+import { ChatDefaultPageComponent } from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
+import { ChatRoomComponent } from './components/main-container/chat-area/chat-room/chat-room.component';
+import { MaterialModule } from './shared/material.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { ChatRoomComponent } from './components/chat-room/chat-room.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    AppRoutingModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
