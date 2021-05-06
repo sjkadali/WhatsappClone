f429d8bb127f350fa531f06abc2cc6520b35e9ec
sjkadali <skadalikat@gmail.com>
2021-05-06 09:41:19 -0400

    Created components, services, guard

--- whatsapp-clone/src/app/app.module.ts
+++ whatsapp-clone/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarContentComponent } from './components/sidebar-content/sidebar-content.component';
+import { ChatAreaComponent } from './components/chat-area/chat-area.component';
+import { ChatDefaultPageComponent } from './components/chat-default-page/chat-default-page.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    SidebarComponent,
+    SidebarContentComponent,
+    ChatAreaComponent,
+    ChatDefaultPageComponent,
+    ChatRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
