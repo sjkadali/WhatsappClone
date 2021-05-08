947d15aecf1c08d89d3aef4a9f75ae253a8a86d6
sjkadali <skadalikat@gmail.com>
2021-05-08 19:06:23 -0400

    updates to routing, chatarea, main-container

--- whatsapp-clone/src/app/components/main-container/chat-area/chat-area.component.ts
+++ whatsapp-clone/src/app/components/main-container/chat-area/chat-area.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chat-area',
   templateUrl: './chat-area.component.html',
-  styleUrls: ['./chat-area.component.css']
+  styleUrls: ['./chat-area.component.scss']
 })
 export class ChatAreaComponent implements OnInit {
 
