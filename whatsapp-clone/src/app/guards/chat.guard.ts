b49967e405852e3d3868f2d2a63c339a083abdd2
sjkadali <skadalikat@gmail.com>
2021-05-13 17:21:50 -0400

    Added login, logout features

--- whatsapp-clone/src/app/guards/chat.guard.ts
+++ whatsapp-clone/src/app/guards/chat.guard.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatGuard implements CanActivate {
+
+  constructor(private afAuth: AngularFireAuth,
+              private router: Router) {
+
+              }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.afAuth.authState
+      .pipe(
+        map(user => user !== null),
+        tap(value => {
+          if (!value) {
+            this.router.navigateByUrl('/login').then();
+            return value;
+          } else {
+            return value;
+          }
+        }));
   }
   
 }
--- whatsapp-clone/src/app/services/common.service.ts
+++ whatsapp-clone/src/app/services/common.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { auth, User } from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
 
-  constructor() { }
+  private pathParamState:BehaviorSubject<string> = new BehaviorSubject<string>('');
+  pathParam: Observable<string>;
+
+  private user: User;
+
+  constructor(private afs: AngularFirestore,
+              private afAuth: AngularFireAuth,
+              private router: Router) { 
+    this.pathParam = this.pathParamState.asObservable();
+    this.afAuth.authState.subscribe( user  => {
+      if (user) {
+        this.user = user;
+        localStorage.set('user', JSON.stringify(this.user));
+        this.router.navigateByUrl('').then();
+      } else {
+        localStorage.setItem('user', null);
+      }
+    });
+  }
+
+  loginWithGoogle(): void {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then((data):void => {
+      if (data.user) {
+        this.user = data.user;
+        localStorage.setItem('user', JSON.stringify(this.user));
+        this.router.navigateByUrl('').then();
+      } else {
+        localStorage.setItem('user', null);
+      }
+    });
+  }
+
+  logout():void {
+    this.afAuth.auth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigateByUrl('/login').then();
+    });
+  }
+
+  updatePathParamState(newPathParam: string):void {
+    this.pathParamState.next(newPathParam);
+  }
+
+  getUser(): User {
+    return this.user;
+  }
 }
